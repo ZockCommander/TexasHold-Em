1932b389aceac6d2c421447b6dcc3ace132b7a02
Alexander Ortner <alexander.ortner@hak-kitz.ac.at>
2021-09-28 12:21:56 +0200

    Projektdateien hinzufügen.

---
+++ TexasHold'Em/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TexasHold_Em
+{
+    static class Program
+    {
+        /// <summary>
+        /// Der Haupteinstiegspunkt für die Anwendung.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
